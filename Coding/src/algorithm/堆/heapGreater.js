31f423fede957e74f8234bc536df68d4cdb6c302
xzp <xingzepeng@geo-compass.com>
2022-08-19 18:01:30 +0800

    加强堆注释

--- Coding/src/algorithm/堆/heapGreater.js
+++ Coding/src/algorithm/堆/heapGreater.js
@@ -7,6 +7,14 @@
 //     this.value = val;
 //   }
 // }
+
+/**
+ *  关于比较函数 
+ *  - 如果 compareFunction(a, b) 小于 0 ，那么 a 会被排列到 b 之前；
+    - 如果 compareFunction(a, b) 等于 0 ， a 和 b 的相对位置不变。备注： ECMAScript 标准并不保证这一行为，而且也不是所有浏览器都会遵守（例如 Mozilla 在 2003 年之前的版本）；
+    - 如果 compareFunction(a, b) 大于 0 ， b 会被排列到 a 之前。
+    compareFunction(a, b) 必须总是对相同的输入返回相同的比较结果，否则排序的结果将是不确定的。
+ */
 class HeapGreater {
   constructor(comparator) {
     this.heap = [];
@@ -54,7 +62,9 @@ class HeapGreater {
     this.heap.slice(index, 1);
     if (replace !== obj) {
       this.heap[index] = replace;
+      //将索引 设置对
       this.indexMap.set(replace, index);
+      //调整堆结构
       this.resign(replace);
     }
   }
